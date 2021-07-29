bcc59ed9a6b438f4cefecaedff769715362152b3
JPPlaysGamer <74308134+JPPlaysGamer@users.noreply.github.com>
2021-07-29 11:57:01 -0300

    Update WebClientExample.java

--- IEIReference/Java/Examples/WebClientExample.java
+++ IEIReference/Java/Examples/WebClientExample.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import com.iei.system.web.*;
+import com.ieireference.system.web.*;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
