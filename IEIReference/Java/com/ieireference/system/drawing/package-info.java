aa23841b1ba5dbd9a9e5d58cb120260fb8f4a060
JPPlaysGamer <74308134+JPPlaysGamer@users.noreply.github.com>
2021-08-06 11:37:03 -0300

    Added Drawing

---
+++ IEIReference/Java/com/ieireference/system/drawing/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * Represents a auto drawing tools for use in GUI. Can be Swing or JavaFX
+ * 
+ * @author JPPlaysGamer (John Paul)
+ *
+ */
+package com.ieireference.system.drawing;
