db5deeaa015a4007e2c9cc0a4b2726c3868d495a
JPPlaysGamer <74308134+JPPlaysGamer@users.noreply.github.com>
2021-08-20 10:31:40 -0300

    Moved to src

--- IEIReference/Java/com/ieireference/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- * The root
- */
-package com.ieireference;
--- IEIReference/Java/com/ieireference/system/io/BufferType.java
+++
@@ -1,8 +0,0 @@
-package com.ieireference.system.io;
-
-/**
- * Represents a enumeration of types of bytes.
- * */
-public enum BufferType {
-	Bytes, Kilobytes, Megabytes, Gigabytes
-}
